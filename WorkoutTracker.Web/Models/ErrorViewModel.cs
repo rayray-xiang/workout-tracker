5f48ad51f3676903ae31ef2d0cdf3d8c87437457
Ray Xiang <rayray.xiang@gmail.com>
2026-09-10 17:55:22 -0700

    first commit

---
+++ WorkoutTracker.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WorkoutTracker.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
